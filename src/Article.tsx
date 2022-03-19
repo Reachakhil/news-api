e4e8a4862b1748be52174afce5fda1430dc0988a
Akhil Bhardwaj <akhilbhardwaj@Akhils-MacBook-Air.local>
2022-03-19 19:28:36 +0530

    second

--- src/Article.tsx
+++ src/Article.tsx
@@ -1,35 +1,35 @@
 
 import './article.css';
 
-function Article(data:any){
-    const parseDate=(date:any)=>{
+function Article(data: any) {
+    const parseDate = (date: any) => {
         const curdate = new Date(date);
         return curdate.toUTCString();
     }
-    return(
-        <>    
-        {
-            data.data.length>0 && data.data.map((tab:any)=>(
-                <div className="article">
-                <div className='article_content'>
-                <div className='article_content_heading'> 
-                {tab.title}
-                </div>
-                <div className='article_content_detail'>
-                    <div>{tab.author}</div>
-                    <div>{parseDate(tab.publishedAt)}</div>
-                </div>
-                <div className='article_content_content'>
-                {tab.content}
-                </div>
-                </div>
-                <div className='article_img'>
-                <img className='article_image' src={tab.urlToImage} alt='' />
-                </div>
-            </div>
-            ))
-            
-}
+    return (
+        <>
+            {
+                data.data.length > 0 && data.data.map((tab: any) => (
+                    <div className="article">
+                        <div className='article_content'>
+                            <div className='article_content_heading'>
+                                {tab.title}
+                            </div>
+                            <div className='article_content_detail'>
+                                <div>{tab.author}</div>
+                                <div>{parseDate(tab.publishedAt)}</div>
+                            </div>
+                            <div className='article_content_content'>
+                                {tab.content}
+                            </div>
+                        </div>
+                        <div className='article_img'>
+                            <img className='article_image' src={tab.urlToImage} alt='' />
+                        </div>
+                    </div>
+                ))
+
+            }
         </>
 
     )
